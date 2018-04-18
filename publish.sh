3ec8bcb727ce9208f78d0e3d03b65de9eecb6eee
David Harrigan <dharrigan118@gmail.com>
2018-04-17 21:14:13 -0400

    update publish scrip to target master

--- publish.sh
+++ publish.sh
@@ -14,8 +14,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public gh-pages
+echo "Checking out master branch into public"
+git worktree add -B master public master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -23,5 +23,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+echo "Updating master branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
